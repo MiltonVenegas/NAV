e3f2f0c9930ead21ba5aae7f8826958947dbb330
MiltonVenegas <101526482+MiltonVenegas@users.noreply.github.com>
2024-02-01 20:19:19 -0600

    Ok

---
+++ Script.js
@@ -0,0 +1,8 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+  });
+
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
